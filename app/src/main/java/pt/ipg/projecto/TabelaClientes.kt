979de90c2c97472ff654585d2278e4aec7d40a00
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2023-05-09 09:52:47 +0100

    TabelaCategorias TabelaClientes TabelaBD

--- app/src/main/java/pt/ipg/projecto/TabelaClientes.kt
+++ app/src/main/java/pt/ipg/projecto/TabelaClientes.kt
@@ -5,7 +5,7 @@ import android.provider.BaseColumns
 
 class TabelaClientes(db: SQLiteDatabase): TabelaBD(db, "Clientes"){
     override fun cria() {
-        db.execSQL("CREAT TABLE $NOME_TABELA($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL),FOREIGN KEY(id_categoria) REFERENCES CATEGORIAS ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID})ON DELETE RESTRICT")
+        db.execSQL("CREATE TABLE $NOME_TABELA($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL),FOREIGN KEY(id_categoria) REFERENCES CATEGORIAS ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID})ON DELETE RESTRICT")
     }
 
     companion object{
