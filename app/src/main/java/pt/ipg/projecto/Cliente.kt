23aee7734f46133f4ff6e6ea9357efd567e646a8
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2023-05-12 09:16:47 +0100

    criar funcao para converter cliente em contentValue

--- app/src/main/java/pt/ipg/projecto/Cliente.kt
+++ app/src/main/java/pt/ipg/projecto/Cliente.kt
@@ -1,11 +1,22 @@
 package pt.ipg.projecto
 
+import android.content.ContentValues
+
 data class Cliente (
     var titulo: String,
-    var 
-    var
-        )
+    var idCategoria: Int,
+    var isbn: String? = null,
+    var id: Long = -1
+        ) {
 
-{
+    fun toContentValues(): ContentValues{
+        val valores = ContentValues()
+
+        valores.put(TabelaClientes.CAMPO_TITULO, titulo)
+        valores.put(TabelaClientes.CAMPO_ISBN, isbn)
+        valores.put(TabelaClientes.CAMPO_FK_CATEGORIA, idCategoria)
+
+        return valores
+    }
 
 }
--- app/src/main/java/pt/ipg/projecto/TabelaClientes.kt
+++ app/src/main/java/pt/ipg/projecto/TabelaClientes.kt
@@ -5,10 +5,13 @@ import android.provider.BaseColumns
 
 class TabelaClientes(db: SQLiteDatabase): TabelaBD(db, "Clientes"){
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL UNIQUE,    FOREIGN KEY(id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_TITULO TEXT NOT NULL, $CAMPO_ISBN TEXT, $CAMPO_FK_CATEGORIA INTEGER NOT NULL UNIQUE,    FOREIGN KEY($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object{
         const val NOME_TABELA = "Clientes"
+        const val CAMPO_TITULO = "titulo"
+        const val CAMPO_ISBN = "isbn"
+        const val CAMPO_FK_CATEGORIA = "id_categoria"
     }
 }
