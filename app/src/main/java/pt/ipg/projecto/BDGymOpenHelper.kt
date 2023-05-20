954b19b657b2addd14506c43a2d28ce47aaaa360
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2023-05-20 16:34:59 +0100

    Update OpenHelper Criar ClientesContentProvider Update testes

--- app/src/main/java/pt/ipg/projecto/BDGymOpenHelper.kt
+++ app/src/main/java/pt/ipg/projecto/BDGymOpenHelper.kt
@@ -4,29 +4,49 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-private const val NOME_BASE_DADOS = "Gym.db"
 private const val VERSAO = 1
 
 class BDGymOpenHelper(
-    context: Context?,
-    factory: SQLiteDatabase.CursorFactory?
-) : SQLiteOpenHelper(context, NOME_BASE_DADOS, factory, VERSAO) {
-
+    context: Context?
+) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO) {
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
     override fun onCreate(db: SQLiteDatabase?) {
         requireNotNull(db)
         TabelaCategorias(db).cria()
         TabelaClientes(db).cria()
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        if (p1 < 2) {
-            TODO("Not yet implemented")
-        }
-
-
-
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     *
+     *
+     *
+     * The SQLite ALTER TABLE documentation can be found
+     * [here](http://sqlite.org/lang_altertable.html). If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     *
+     *
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     *
+     *
+     * @param db The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
     }
+
     companion object {
-        const val NOME_BASE_DADOS = "Clientes.bd"
+        const val NOME_BASE_DADOS = "clientes.db"
     }
 }
